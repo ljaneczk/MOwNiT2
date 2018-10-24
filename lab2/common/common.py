1982e07c33d7e9f75347bbcb33072bf975aad3ae
Łukasz Janeczko <lukij1997@gmail.com>
2018-10-24 14:04:02 +0200

    Updated common functions

--- lab2/common/common.py
+++ lab2/common/common.py
@@ -25,5 +25,15 @@ def get_value(string):
         return string
 
 
-def round_ft_vector(dft_result, k=2):
-    return [(round(get_value(np.real(value)), k) + round(get_value(np.imag(value)), k) * 1j) for value in dft_result]
+def round_ft_vector(dft_result, round_to=2):
+    return [(round(get_value(np.real(value)), round_to) + round(get_value(np.imag(value)), round_to) * 1j) for value in dft_result]
+
+
+def get_fourier_transformation_input():
+    split_line = input().split()
+    n = get_value(split_line[0])
+    row = [get_value(x) for x in split_line[1:]]
+    while len(row) < n:
+        for string in input().split():
+            row.append(get_value(string))
+    return n, row[:n]
