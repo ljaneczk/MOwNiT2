d373346a74d0a72491001a5a74b956e4d266661f
Łukasz Janeczko <lukij1997@gmail.com>
2018-11-03 11:24:53 +0100

    Created common python file with important functions

---
+++ lab3/common.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def get_nd_array(a):
+    if isinstance(a, np.matrix):
+        return a.A
+    else:
+        return a
+
+
+def convert_nd_array_to_matrix(a):
+    return np.matrix(a)
+
+
+def convert_matrix_to_nd_array(a: np.matrix):
+    return a.A
+
+
+def read_nd_array_from_input(message="", dtype=float):
+    n, m = map(int, input(message).split())
+    a = np.zeros((n, m), dtype=dtype)
+    for i in range(n):
+        a[i] = [dtype(x) for x in input().split()]
+    return a
